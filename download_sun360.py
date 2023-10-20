f57c7d88e22ffd3aaff074a46472df9705c76a4f
Kai Henning <khenning@math.tu-berlin.de>
2023-10-20 16:00:37 +0200

    update

--- download_sun360.py
+++ download_sun360.py
@@ -2,4 +2,4 @@ import gdown
 
 url = 'https://drive.google.com/drive/folders/1ooaYwvNuFd-iEEcmOQHpLunJEmo7b4NM'
 output = 'SUN360.zip'
-gdown.download(url, output, quiet=False) 
+gdown.download(url, output, quiet=False, fuzzy=True) 
